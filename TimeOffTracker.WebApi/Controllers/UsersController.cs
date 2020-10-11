285ee8e26d4589f14614fa45058b4f7f07e2bb02
PedenkoIllia <pedenkoia@gmail.com>
2020-10-11 15:27:39 +0300

    Add user controller

---
+++ TimeOffTracker.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,47 @@
+﻿using ApiModels.Models;
+using Domain.EF_Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using TimeOffTracker.WebApi.ViewModels;
+
+namespace TimeOffTracker.WebApi.Controllers
+{
+
+    [ApiController]
+    [Route("auth/[controller]")]
+    public class UsersController : BaseController
+    {
+        private readonly UserManager<User> _userManager;
+        private ILogger<UsersController> _logger;
+
+        public UsersController(UserManager<User> userManager, ILogger<UsersController> logger)
+        {
+            _userManager = userManager;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public UserApiModel GetUser(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        [Authorize(Roles = "Admin")]
+        public UserApiModel ChangeUserRole([FromForm] RoleChangeModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        [Authorize(Roles = "Admin")]
+        public IActionResult DeleteUser([FromForm] int userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
