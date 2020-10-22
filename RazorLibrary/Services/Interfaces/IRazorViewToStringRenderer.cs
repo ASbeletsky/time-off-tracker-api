9384483e79cfe03eb61f47c4b7fd49a6d1712790
PedenkoIllia <pedenkoia@gmail.com>
2020-10-22 18:46:04 +0300

    Add email render + localization

---
+++ RazorLibrary/Services/Interfaces/IRazorViewToStringRenderer.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewEngines;
+using System.Threading.Tasks;
+
+namespace RazorLibrary.Services.Interfaces
+{
+    public interface IRazorViewToStringRenderer
+    {
+        Task<string> RenderViewToStringAsync<TModel>(string viewName, TModel model);
+    }
+}
