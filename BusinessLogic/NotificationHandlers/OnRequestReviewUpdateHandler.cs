1f59a47194074e8aecfb0eb1a20302bca5d10395
Tsypchenko Yulia <yutsypa@icloud.com>
2020-10-31 16:00:18 +0300

    fix

--- BusinessLogic/NotificationHandlers/OnRequestReviewUpdateHandler.cs
+++ BusinessLogic/NotificationHandlers/OnRequestReviewUpdateHandler.cs
@@ -28,15 +28,17 @@ namespace BusinessLogic.NotificationHandlers
         {
             var requestfromDb = await _requestRepository.FindAsync(x => x.Id == notification.Request.Id);
 
-            if (requestfromDb.Reviews.All(x => x.IsApproved == true))
+            if(requestfromDb.Reviews.Any(x => x.IsApproved == null))
+                requestfromDb.State = VacationRequestState.InProgress;
+            else if(requestfromDb.Reviews.All(x => x.IsApproved == true))
                 requestfromDb.State = VacationRequestState.Approved;
             else if(requestfromDb.Reviews.Any(x => x.IsApproved == false))
                 requestfromDb.State = VacationRequestState.Rejected;
 
             await _requestRepository.UpdateAsync(requestfromDb);
 
-            var notification_approved = new RequestApprovedNotification { Request = await _requestRepository.FindAsync(notification.Request.Id) };
-            await _mediator.Publish(notification_approved);
+            //var notification_approved = new RequestApprovedNotification { Request = await _requestRepository.FindAsync(notification.Request.Id) };
+            //await _mediator.Publish(notification_approved);
         }
     }
 }
