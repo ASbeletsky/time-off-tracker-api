b5bf83554fc0fa20fcf249bbc24ef1402b45fa13
Tsypchenko Yulia <yutsypa@icloud.com>
2020-11-21 18:11:46 +0300

    added assembly for tests

---
+++ TimeOffTrackerTests/ServicesTests/TimeOffRequestServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TimeOffTrackerTests.ServicesTests
+{
+    public class TimeOffRequestServiceTest
+    {
+        [Fact]
+        public void Method()
+        {
+            // Arrange
+           
+
+            // Act
+           
+
+            // Assert
+            
+        }
+    }
+}
